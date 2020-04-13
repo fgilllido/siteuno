ab982594f59e7691761beaf1bec1557c09ca91e2
manolo.gil <manolo.gil@northgateplc.es>
2020-04-13 20:49:20 +0200

    Vista vhora.html

--- hola/models.py
+++ hola/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=200)
+    fec_nac = models.DateField()
+
+    def __str__(self):
+            return self.nombre
+    
--- hola/views.py
+++ hola/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import datetime
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hola, este es la base de la Aplicacion llamada <hola>")
 
+def lahora(request):
+
+    dt = datetime.datetime.now()
+    context = {"dt": dt}
+
+    return render(request, 'vhora.html', context)
