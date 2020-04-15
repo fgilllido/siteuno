d1784df152ec061563eb2d63359347632d72ef74
manolo.gil <manolo.gil@northgateplc.es>
2020-04-15 10:15:40 +0200

    Vistas adicionales para <polls>

--- polls/views.py
+++ polls/views.py
@@ -6,5 +6,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("You're at the polls index.")
+    return HttpResponse("Indice de APP de encuestas (Polls).")
 
+def detail(request, question_id):
+    return HttpResponse("Consultando la Pregunta: %s" % question_id)
+
+def results(request, question_id):
+    response = "Resultados a la Pregunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Has Votado para la Pregunta %s." % question_id)
