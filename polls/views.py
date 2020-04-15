8bf7cf920c4634f3ed692a0feae443f3e10e1762
manolo.gil <manolo.gil@northgateplc.es>
2020-04-15 14:32:04 +0200

    Indice de encuestas

--- polls/views.py
+++ polls/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Indice de APP de encuestas (Polls).")
+    # return HttpResponse("Indice de APP de encuestas (Polls).")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    return render(request,'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("Consultando la Pregunta: %s" % question_id)
