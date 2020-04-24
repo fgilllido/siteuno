9beffab2e0e957b9b369ebcf514b87c4229d63d2
manolo.gil <manolo.gil@northgateplc.es>
2020-04-24 14:53:22 +0200

    Cambiando la vista del admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
-admin.site.register(Question)
+from .models import Question, Choice
 
+#admin.site.register(Question)
+admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
